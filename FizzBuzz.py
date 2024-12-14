d217431dac656a3104d77830343d52b10bbfe4d7
3p1c-r <aakashpathak9876@gmail.com>
2024-12-14 12:49:23 -0500

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,1 +1,25 @@
+def fizz_buzzV1(num):
+    if num % 3 == 0 and num % 5 != 0:
+        print("Fizz")
+    elif num % 5 == 0 and num % 3 != 0:
+        print("Buzz")
+    elif num % 15 == 0:
+        print("FizzBuzz")
+    else:
+        print(str(num))
+        
+fizz_buzzV1(6)
+fizz_buzzV1(10)
+fizz_buzzV1(30)
 
+def fizz_buzzV2(num):
+    for i in range(0,num + 1):
+        if i % 3 == 0 and i % 5 != 0:
+            print("Fizz")
+        elif i % 5 == 0 and i % 3 != 0:
+            print("Buzz")
+        elif i % 15 == 0:
+            print("FizzBuzz")
+        else: 
+            print(i)
+fizz_buzzV2(10)
